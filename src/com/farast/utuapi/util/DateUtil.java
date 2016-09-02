ebc3b5cd34b528030290c6ae774d780362f02e2b
Čeněk Drbal <cendrb@gmail.com>
2016-09-02 08:20:33 +0200

    add zero time method helper

--- src/com/farast/utuapi/util/DateUtil.java
+++ src/com/farast/utuapi/util/DateUtil.java
@@ -1,6 +1,8 @@
 package com.farast.utuapi.util;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Created by cendr_000 on 26.07.2016.
@@ -15,4 +17,15 @@ public final class DateUtil {
     public static final SimpleDateFormat CZ_SHORT_DATE_FORMAT = new SimpleDateFormat("dd. MM.");
     public static final SimpleDateFormat CZ_DATE_FORMAT = new SimpleDateFormat("dd. MM. yyyy");
     public static final SimpleDateFormat CZ_WEEK_DATE_FORMAT = new SimpleDateFormat("EEE dd. MM.");
+    public static Date getZeroTimeCurrentDate()
+    {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        return calendar.getTime();
+    }
 }
