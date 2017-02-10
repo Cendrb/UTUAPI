7b65e5f25d13da9a14afa8c6d65adc096e4cfce2
Cendrb <cendrb@gmail.com>
2017-02-10 08:22:45 +0100

    Add time format

--- src/com/farast/utuapi/util/DateUtil.java
+++ src/com/farast/utuapi/util/DateUtil.java
@@ -17,6 +17,7 @@ public final class DateUtil {
     public static final SimpleDateFormat CZ_SHORT_DATE_FORMAT = new SimpleDateFormat("dd. MM.");
     public static final SimpleDateFormat CZ_DATE_FORMAT = new SimpleDateFormat("dd. MM. yyyy");
     public static final SimpleDateFormat CZ_WEEK_DATE_FORMAT = new SimpleDateFormat("EEE dd. MM.");
+    public static final SimpleDateFormat TIME_FORMAT = new SimpleDateFormat("HH:mm");
     public static Date getZeroTimeCurrentDate()
     {
         Calendar calendar = Calendar.getInstance();
